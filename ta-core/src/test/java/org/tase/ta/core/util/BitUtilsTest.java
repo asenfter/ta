71ffd442479d224740bb759bef832808f5c3a61f
Andreas Senfter <asenfter@picturesafe.de>
2016-08-03 10:27:17 +0200

    bit mask comparison

--- ta-core/src/test/java/org/tase/ta/core/util/BitUtilsTest.java
+++ ta-core/src/test/java/org/tase/ta/core/util/BitUtilsTest.java
@@ -1,5 +1,6 @@
 package org.tase.ta.core.util;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,15 +12,16 @@ public class BitUtilsTest {
 
     @Test
     public void test() {
-        doTest("textanalytics", "exactly");
-        doTest("vienna", "naive");
-        doTest("ocean", "canoe");
-        doTest("computerscience", "competencies");
+        doTest("textanalytics", "exactly", 63);
+        doTest("vienna", "naive", 91);
+        doTest("ocean", "canoe", 100);
+        doTest("computerscience", "competencies", 81);
     }
 
-    public void doTest(String string1, String string2) {
+    public void doTest(String string1, String string2, int value) {
         final BitMaskComparator comparator = new BitMaskComparator();
         final int score = comparator.compare(string1, string2);
+        Assert.assertEquals(value, score);
         LOGGER.debug("word1={}, word2={}, score={}", string1, string2, score);
     }
 }
